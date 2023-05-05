71290562c4f0256fe6f1c6fc34c4c23f3f5e2fff
Aradhya <aradhya@my.yorku.ca>
2023-05-05 09:22:58 -0400

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const connectDB = require('./db');
+require('dotenv').config();
+
+const app = express();
+connectDB();
+
+// GraphQL setup will come here later
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1);
+  }
+}
+
+module.exports = connectDB;
