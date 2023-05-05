bcb0a52b781052f3990efec9d9f762f25d4d87fa
Aradhya <aradhya@my.yorku.ca>
2023-05-05 15:03:13 -0400

    Initial

--- src/db/index.js
+++ src/db/index.js
@@ -5,8 +5,6 @@ async function connectDB() {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
     });
     console.log('Connected to MongoDB');
   } catch (error) {
