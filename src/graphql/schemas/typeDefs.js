621cc3d46447b379c61d7ecfcb0a5600dd7de3f1
Aradhya <aradhya@my.yorku.ca>
2023-05-10 13:07:55 -0400

    graphql

--- src/graphql/schemas/typeDefs.js
+++ src/graphql/schemas/typeDefs.js
@@ -27,6 +27,66 @@ const typeDefs = gql `
         path: String!
         method: String!
         parameters: [Parameter]
+        response: [Response]
+    }
+
+    type Parameter {
+        name: String!
+        type: String!
+        required: Boolean!
+    }
+
+    type Response {
+        key: String!
+        type: String!
+    }
+
+    type AuthPayload {
+        token: String!
+        user: User!
+    }
+
+    input ProjectInput {
+        name: String!
+    }
+
+    input ApiConfigInput {
+        endpoints: [EndpointInput]!
+    }
+
+    input EndpointInput {
+        path: String!
+        methid: String!
+        parameters: [ParameterInput]
+        response: [ResponseInput]
+    }
+
+    input ParameterInput {
+        name: String!
+        type: String!
+        required: Boolean!
+    }
+
+    input ResponseInput {
+        key: String!
+        type: String!
+    }
+
+    type Query {
+        me: Userproject(id: ID!): Project
+        projects: [Project]
+        apiConfig(id: ID!): ApiConfig
+        apiCOnfigs: [ApiConfig]
+    }
+
+    type Mutation {
+        signUp(name: String!, email: String!, password:String!): AuthPayload!
+        signIn(email: String!, password: String!): AuthPayload!
+        createProject(input: ProjectInput!): Project!
+        updateProject(id: ID!, input: ProjectInput!): Project!
+        deleteProject(id: ID!): Project!
+        createApiConfig(projectId: ID!, input: ApiConfigInput!): ApiConfig!
+        updateApiConfig(id: ID!)
     }
 
 `
