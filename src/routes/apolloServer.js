5b69b084e2807db9541d6dd21e6911cfb29b4df2
Aradhya <aradhya@my.yorku.ca>
2023-05-11 10:28:58 -0400

    utils

--- src/routes/apolloServer.js
+++ src/routes/apolloServer.js
@@ -1,17 +1,17 @@
 const { ApolloServer } = require('apollo-server-express');
-const typeDefs = require('../graphql/schemas/typeDefs');
-const resolvers = require('../graphql/resolvers/resolvers');
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+const { getUserIdFromToken } = require('./services/AuthService');
 
 const apolloServer = new ApolloServer({
-    typeDefs, resolvers,
-    context : ({ req }) => {
-        const token = req.headers.authorization || '';
+  typeDefs,
+  resolvers,
+  context: ({ req }) => {
+    const token = req.headers.authorization || '';
+    const userId = getUserIdFromToken(token);
 
-        const userId = AuthService.getUserIdFromToken(token);
-
-        return { userId }; 
-    },
+    return { userId };
+  },
 });
 
 module.exports = apolloServer;
-
