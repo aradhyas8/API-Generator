0eb37ae69cd14a894d00a7317662edd4896c7bf9
Aradhya <aradhya@my.yorku.ca>
2023-05-18 10:08:12 -0400

    Backend Fixed

--- src/graphql/schemas/typeDefs.js
+++ src/graphql/schemas/typeDefs.js
@@ -1,96 +1,109 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
-const typeDefs = gql `
+const typeDefs = gql`
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    password: String!
+    projects: [Project]
+  }
 
-    type User {
-        id : ID!
-        name: String!
-        email: String!
-        password: String!
-        projects: [Project]
-    }
+  type Project {
+    id: ID!
+    name: String!
+    user: User!
+    apiConfigs: [ApiConfig]
+  }
 
-    type Project {
-        id: ID!
-        name: String!
-        user: User!
-        apiConfigs: [ApiConfig]
-    }
+  type ApiConfig {
+    id: ID!
+    project: Project!
+    endpoints: [Endpoint]
+  }
 
-    type ApiConfig {
-        id: ID!
-        project: Project!
-        endpoints: [Endpoint]
-    }
+  type Endpoint {
+    path: String!
+    method: String!
+    parameters: [Parameter]
+    response: [Response]
+  }
 
-    type Endpoint {
-        path: String!
-        method: String!
-        parameters: [Parameter]
-        response: [Response]
-    }
+  type Parameter {
+    name: String!
+    type: String!
+    required: Boolean!
+  }
 
-    type Parameter {
-        name: String!
-        type: String!
-        required: Boolean!
-    }
+  type Response {
+    key: String!
+    type: String!
+  }
 
-    type Response {
-        key: String!
-        type: String!
-    }
+  type AuthPayload {
+    token: String!
+    user: User!
+  }
 
-    type AuthPayload {
-        token: String!
-        user: User!
-    }
+  type PropertyInput {
+    key: String!
+    value: String
+    type: String!
+  }
 
-    input ProjectInput {
-        name: String!
-    }
+  input ProjectInput {
+    name: String!
+    description: String
+  }
 
-    input ApiConfigInput {
-        endpoints: [EndpointInput]!
-        projectName: String!
-    }
+  input ApiConfigInput {
+    endpoints: [EndpointInput]!
+    projectName: String!
+  }
 
-    input EndpointInput {
-        path: String!
-        method: String!
-        parameters: [ParameterInput]
-        response: [ResponseInput]
-    }
+  input EndpointInput {
+    path: String!
+    method: String!
+    parameters: [ParameterInput]
+    response: [ResponseInput]
+  }
 
-    input ParameterInput {
-        name: String!
-        type: String!
-        required: Boolean!
-    }
+  input ParameterInput {
+    name: String!
+    type: String!
+    required: Boolean!
+  }
 
-    input ResponseInput {
-        key: String!
-        type: String!
-    }
+  input ResponseInput {
+    key: String!
+    type: String!
+    properties: [PropertyInput]
+  }
 
-    type Query {
-        me: User project(id: ID!): Project
-        projects: [Project]
-        apiConfig(id: ID!): ApiConfig
-        apiConfigs: [ApiConfig]
-    }
+  input PropertyInput {
+    key: String!
+    value: String
+    type: String!
+  }
 
-    type Mutation {
-        signUp(name: String!, email: String!, password:String!): AuthPayload!
-        signIn(email: String!, password: String!): AuthPayload!
-        createProject(input: ProjectInput!): Project!
-        updateProject(id: ID!, input: ProjectInput!): Project!
-        deleteProject(id: ID!): Project!
-        createApiConfig(projectId: ID!, input: ApiConfigInput!): ApiConfig!
-        updateApiConfig(id: ID!, input: ApiConfigInput!): ApiConfig!
-        deleteApiConfig(id: ID!): ApiConfig!
-    }
+  type Query {
+    me: User
+    project(id: ID!): Project
+    projects: [Project]
+    apiConfig(id: ID!): ApiConfig
+    apiConfigs: [ApiConfig]
+  }
 
+  type Mutation {
+    signUp(name: String!, email: String!, password: String!): AuthPayload!
+    signIn(email: String!, password: String!): AuthPayload!
+    createProject(input: ProjectInput!): Project!
+    updateProject(id: ID!, input: ProjectInput!): Project!
+    deleteProject(id: ID!): Project!
+    createApiConfig(projectId: ID!, input: ApiConfigInput!): ApiConfig!
+    updateApiConfig(id: ID!, input: ApiConfigInput!): ApiConfig!
+    deleteApiConfig(id: ID!): ApiConfig!
+  }
 `;
 
 module.exports = typeDefs;
