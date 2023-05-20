3d1ccd7c1a6c7f83b87849ef3fb780236dc40054
Aradhya <aradhya@my.yorku.ca>
2023-05-20 15:16:52 -0400

    FE

--- frontend/ai_api/src/App.js
+++ frontend/ai_api/src/App.js
@@ -1,4 +1,11 @@
 import Home from './components/Home'
+import APIPage from './components/APIPage'
+import Dashboard from './components/Dashboard'
+import DocumentationPage from './components/DocumentationPage'
+import ErrorPage from './components/ErrorPage'
+import LoginPage from './components/LoginPage'
+import RegisterPage from './components/RegisterPage'
+import Settings from './components/Settings'
 import React from 'react'
 
 const App = () => {
