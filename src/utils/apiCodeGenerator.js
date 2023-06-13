9df1f856670dd9c1e201b057db3bd70d0dd4ff4a
Aradhya <aradhya@my.yorku.ca>
2023-06-13 10:28:42 -0400

    CodeGen

--- src/utils/apiCodeGenerator.js
+++ src/utils/apiCodeGenerator.js
@@ -1,93 +1,43 @@
-const generateEndpointCode = (config, endpoint, lang) => {
-    const { method, path, parameters } = endpoint;
-    const isTypescript = lang === "typescript";
-  
-    let code = `// ${method.toUpperCase()} ${path}\n`;
-  
-    const functionName = `${method}${path.replace(/\//g, "_")}`;
-    const paramsType = isTypescript ? ": Params" : "";
-    code += `async function ${functionName}(params${paramsType}) {\n`;
-  
-    code += "  const response = await fetch(`";
-    code += `${config.baseURL}${path.replace(/:[a-zA-Z0-9]+/g, (param) => "${params" + param.substr(1) + "}")}`;
-    code += "`, {\n";
-    code += `    method: "${method.toUpperCase()}",\n`;
-    code += "    headers: {\n";
-  
-    if (parameters.header) {
-      parameters.header.forEach((headerParam) => {
-        code += `      "${headerParam.name}": params.${headerParam.name},\n`;
-      });
+function apiCodeGenerator(apiConfigs) {
+  let code = `
+    const express = require('express');
+    const app = express();
+
+    app.use(express.json());
+  `;
+
+  for (let config of apiConfigs) {
+
+    const endpoint = config.endpoint;
+    const method = config.method;
+    const parameters = config.parameters;
+    const response = config.response;
+
+    let pramasCode = '';
+    for(let param of parameters) {
+      paramsCode += `
+      let ${param.name} = req.paramas.${param.name};
+      `;
     }
-  
-    code += "    },\n";
-  
-    if (method !== "get" && parameters.body) {
-      code += "    body: JSON.stringify(params.body),\n";
+
+    code += `
+        app.${method.toLowerCase()}('${endpoint}', (req, res) => {
+            ${paramsCode}
+
+            // Mock response
+            res.json(${JSON.stringify(response)});
+        });
+        `;
     }
-  
-    code += "  });\n";
-    code += "  return response.json();\n";
-    code += "}\n\n";
-  
+
+    code += `
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => console.log(\`Server running on port \${PORT}\`));
+    `;
+
     return code;
-  };
-  
-  const generateJavascriptCode = (apiConfigs) => {
-    let code = "";
-  
-    apiConfigs.forEach((config) => {
-      const { endpoints } = config;
-  
-      endpoints.forEach((endpoint) => {
-        code += generateEndpointCode(config, endpoint, "javascript");
-      });
-    });
-  
-    return code;
-  };
-  
-  const generateTypescriptCode = (apiConfigs) => {
-    let code = "";
-  
-    apiConfigs.forEach((config) => {
-      const { endpoints } = config;
-  
-      endpoints.forEach((endpoint) => {
-        const { parameters } = endpoint;
-  
-        if (parameters) {
-          code += `interface Params {\n`;
-  
-          if (parameters.path) {
-            parameters.path.forEach((pathParam) => {
-              code += `  ${pathParam.name}: string;\n`;
-            });
-          }
-  
-          if (parameters.header) {
-            parameters.header.forEach((headerParam) => {
-              code += `  ${headerParam.name}: string;\n`;
-            });
-          }
-  
-          if (endpoint.method !== "get" && parameters.body) {
-            code += `  body: {\n`;
-            parameters.body.forEach((bodyParam) => {
-              code += `    ${bodyParam.name}: ${bodyParam.type};\n`;
-            });
-            code += `  };\n`;
-          }
-  
-          code += "}\n\n";
-        }
-  
-        code += generateEndpointCode(config, endpoint, "typescript");
-      });
-    });
-  
-    return code;
-  };
-  
-  module.exports = { generateJavascriptCode, generateTypescriptCode };
-  
+}
+
+module.exports = {
+    apiCodeGenerator,
+};
